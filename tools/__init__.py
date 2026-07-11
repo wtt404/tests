72e3574139b42e69700b08d0017c05eefab2d3aa
wtt404 <137714878+wtt404@users.noreply.github.com>
2026-07-11 13:57:31 +0300

    Update __init__.py

--- tools/__init__.py
+++ tools/__init__.py
@@ -2,6 +2,5 @@ from tools.translate import TranslateTool
 from tools.summarize import SummarizeTool
 
 TOOLS = {
-    "translate": TranslateTool(),
-    "summarize": SummarizeTool(),
+    "summarize": SummarizeTool()
 }
