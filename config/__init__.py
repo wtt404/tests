9114f540e6531ba9e7c0e75e216e27c459634921
wtt404 <137714878+wtt404@users.noreply.github.com>
2026-07-20 02:16:51 +0300

    Update __init__.py

--- config/__init__.py
+++ config/__init__.py
@@ -6,5 +6,3 @@ from . import settings
 load_dotenv()
 
 DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
-OPENROUTER_API_KEY = os.getenv("OPENROUTER_API_KEY")
-OCR_SPACE_API_KEY = os.getenv("OCR_SPACE_API_KEY")
